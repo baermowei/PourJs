30a6eb7d3de08699f991e6b8e2e976dac0b24ce2
baermowei <1255228914@qq.com>
2015-12-15 16:41:35 +0800

    制作了轮播效果
    1,找图修图弄了很久,,想找一些背景色比较纯的图片,这样在切换的时候就不会因为背景而显得突兀,其实最好的方式是应用渐变,可是我懒啊,而且这个项目不打算使用css3的,整体效果还能过得去
    2,采用之前学习的时候做的运动框架,挺好用的,还省得自己写,制作的比较简单;
    3,修改了一下样式,本来打算用黑白灰为色调,因为图片用的都是黑色调,不明显,于是该用绿色了,先这样,如果有更合适的颜色,再修改

--- Js/move.js
+++ Js/move.js
@@ -1,25 +1,74 @@
+	var zidong=null;
+	var inow=0;
 	window.onload=function(){
+	
 	var oUl=document.getElementById("nav_ul");
 	var aa=oUl.getElementsByTagName('a');
 	var oLi=document.getElementById('move_li');
-	for (var i = 0; i < aa.length; i++) {
+	var obar=document.getElementById('bar');
+	var obig_ul=document.getElementById('big_pic');
+	var abig_li=obig_ul.getElementsByTagName('li');
+	var osmall_ul=document.getElementById('samll_ul');
+	var asmall_li=osmall_ul.getElementsByTagName('li');
+	var bar_bg=['rgb(34,34,34)','#fff','#000','rgb(15,25,25)','rgb(25,25,25)'];
 
-		aa[i].onmouseover=function(){
-			oLi_Move(oLi,this.offsetLeft);
-			this.style.color='#111';
-		}
-		aa[i].onmouseout=function(){
-			this.style.color='#777';
-		}
-	}
+		for (var i = 0; i < aa.length; i++) {
 
-	}
+				aa[i].onmouseover=function(){
+					oLi_Move(oLi,this.offsetLeft);
+					this.style.color='#111';
+				}
+				aa[i].onmouseout=function(){
+					this.style.color='#777';
+				}
+			}
+
 		
-var ispeed=0;
+	for (var i = 0; i < asmall_li.length; i++) {
+		asmall_li[i].index=i;
+		asmall_li[i].onclick=function(){
+
+				for (var j = 0; j < abig_li.length; j++) 
+				{
+				moveStart(abig_li[j],{"opacity":0})
+				asmall_li[j].className='';
+				}
+			moveStart(abig_li[this.index],{"opacity":100});
+			this.className='active';
+			obar.style.backgroundColor=bar_bg[this.index];
+		};
+			
+	};
+
+	function autoMove(){
+		
+		if (inow==asmall_li.length-1) {
+			inow=0;
+		}
+		else{
+			inow++;
+		}
+
+		for (var i = 0; i < asmall_li.length; i++) {
+			asmall_li[i].className='';
+			moveStart(abig_li[i],{"opacity":0})
+		}
+
+		asmall_li[inow].className='active';
+		moveStart(abig_li[inow],{"opacity":100});
+		obar.style.backgroundColor=bar_bg[inow];
+
+	}	 
+			
+		
+ zidong=setInterval(autoMove,3000);	
+
+}
+		
+		var ispeed=0;
 
 
 		function oLi_Move(obj,itarget){
-
 			clearInterval(obj.timer)
 			obj.timer=setInterval(function(){
 				ispeed+=(itarget-obj.offsetLeft)/5;
@@ -36,3 +85,82 @@ var ispeed=0;
 			},30)
 
 		}
+
+
+function getByClass(Oparent,iName )
+	{
+		var oall=document.getElementsByTagName('*')
+		var arr=[];
+		for (var i = 0; i < oall.length; i++)
+		 {
+			if (oall[i].className==iName)
+			 {
+				arr.push(oall[i])
+			}
+
+		}
+		return arr;
+	}
+
+		function getStyle(obj,attr) 
+		{
+					if (obj.currentStyle) 
+					{
+						return obj.currentStyle[attr];
+					} else {
+						return getComputedStyle(obj,false)[attr];
+					}
+		}
+
+
+
+		function moveStart(obj,json,fn) 
+		{
+			clearInterval(obj.timer);
+
+		obj.timer=setInterval(function()
+			{	var istop=true;
+				for(attr in json)
+				{
+
+					var icurrent=0;											
+					if (attr=="opacity")									//因为透明度比较特殊，所以特别判断
+					 {
+						icurrent=parseInt(parseFloat(getStyle(obj,attr))*100);
+					} else
+					 {
+						icurrent=parseInt(getStyle(obj,attr));
+					}
+
+					var itarget=json[attr];
+					var ispeed=(itarget- icurrent)/8;
+					ispeed=ispeed>0?Math.ceil(ispeed):Math.floor(ispeed);
+
+
+					if (icurrent!=itarget) 
+					{
+						istop=false;
+					}
+				
+						if (attr=="opacity") 
+						{
+							obj.style.opacity=(icurrent+ispeed)/100;
+							obj.style.fliter="alpha(opacity:"+icurrent+ispeed+")";
+						} else 
+						{
+							obj.style[attr]=icurrent+ispeed+"px";
+						}
+
+				}
+				
+				if (istop) 
+				{
+					clearInterval(obj.timer);
+					if (fn) 
+					{
+						fn();
+					}
+				}
+			},30)	
+		}
+
